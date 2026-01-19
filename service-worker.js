1779e321e83ab33cd1238da7dbed39d92f45b254
aprujabegum9-cell <aprujabegum9@gmail.com>
2026-01-19 10:42:12 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,23 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyD_TytbX1kd4SKQ5uHZfd5XVGwdw0gzUOE",
+    authDomain: "feeling-hungry-102a9.firebaseapp.com",
+    databaseURL: "https://feeling-hungry-102a9-default-rtdb.firebaseio.com",
+    projectId: "feeling-hungry-102a9",
+    storageBucket: "feeling-hungry-102a9.appspot.com",
+    messagingSenderId: "736201265500",
+    appId: "1:736201265500:web:7b7064b314247c02ad7199"
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
+const messaging = firebase.messaging();
 
-self.addEventListener('push', event => {
-  let data = {};
-  try {
-    data = event.data ? event.data.json() : {};
-  } catch (e) {
-    data = {};
-  }
-
-  const title = data.title || 'New Order!';
-  const options = {
-    body: data.message || 'You have a new order.',
-    icon: '/icon-192.png',
-    badge: '/icon-192.png'
-  };
-
-  event.waitUntil(
-    self.registration.showNotification(title, options)
-  );
-});
-
-self.addEventListener('notificationclick', event => {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow('/')
-  );
+messaging.onBackgroundMessage(function(payload) {
+    console.log('[SW] Background Message', payload);
+    const { title, body, icon } = payload.notification;
+    self.registration.showNotification(title, {
+        body: body,
+        icon: icon || 'icon-192.png'
+    });
 });
